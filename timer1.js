375ff2dad7d59df952b0c183571d4c31e6e71ef6
Ian-c-cameron <ian.c.cam@gmail.com>
2020-09-22 20:49:27 +0000

    timer1.js - set alarm timers with command line arguments

---
+++ timer1.js
@@ -0,0 +1,11 @@
+let args = process.argv.slice(2);
+const soundAlarm = () => {
+  console.log("ALARM!!!");
+};
+
+for (let i = args.length - 1; i >= 0; i--) {
+  let time = Number(args[i]);
+  if (time) {
+    setTimeout(soundAlarm, time * 1000);
+  }
+}
